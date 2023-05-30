015f8541bbd545ab3d41587cb57ae9d4d1726962
Juan Diego Salvador <juandiegosalvador@gmail.com>
2023-05-29 23:56:42 -0500

    FINAL-CLASE

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './Pages/App'
 import './index.css'
+import Layout from './Components/Layout'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Layout>
+      <App />
+    </Layout>
   </React.StrictMode>,
 )
