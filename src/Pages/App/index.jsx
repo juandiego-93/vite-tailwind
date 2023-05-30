c74220d5f62c108e4a51d1175772af465874159a
Juan Diego Salvador <juandiegosalvador@gmail.com>
2023-05-29 20:09:37 -0500

    Agregados estilos al navbar

--- src/Pages/App/index.jsx
+++ src/Pages/App/index.jsx
@@ -1,21 +1,33 @@
+import { useRoutes, BrowserRouter } from 'react-router-dom'
 import Home from '../Home'
 import MyAccount from '../MyAccount'
 import MyOrder from '../MyOrder'
 import MyOrders from '../MyOrders'
 import NotFound from '../NotFound'
 import SignIn from '../SignIn'
+import Navbar from '../../Components/Navbar'
 import './App.css'
 
+function AppRoutes() {
+  let routes = useRoutes([
+    { path:'/', element: <Home />},
+    { path:'/my-account', element: <MyAccount />},
+    { path:'/my-order', element: <MyOrder />},
+    { path:'/my-orders', element: <MyOrders />},
+    { path:'/*', element: <NotFound />},
+    { path:'/sign-in', element: <SignIn />},
+  ])
+
+  return routes
+}
+
 function App() {
+
   return (
-<div className='bg-red-100'>
-  <Home />
-  <MyAccount />
-  <MyOrder />
-  <MyOrders />
-  <NotFound />
-  <SignIn />
-</div>
+    <BrowserRouter>
+      <AppRoutes />
+      <Navbar />
+    </BrowserRouter>
   )
 }
 
