1ff93c3676a47e68835b67827b2ce879fc015e9b
Juan Diego Salvador <juandiegosalvador@gmail.com>
2023-05-30 19:52:49 -0500

    initial

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,7 +1,15 @@
+import React, { useEffect } from 'react'
 import Layout from '../../Components/Layout'
 import Card from '../../Components/Card'
 
 function Home() {
+    const[items, setItems] = React.useState(null) 
+
+    React.useEffect(() => {
+        fetch('https://api.escuelajs.co/api/v1/products')
+            .then(response => console.log(response.json()))
+    }, [])
+
     return (
         <Layout>
             Home
