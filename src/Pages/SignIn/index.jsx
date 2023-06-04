da0b7cf118bb0d716615cd50168dddfaf1061a94
Juan Diego Salvador <juandiegosalvador@gmail.com>
2023-06-04 13:09:52 -0500

    final-reto5

--- src/Pages/SignIn/index.jsx
+++ src/Pages/SignIn/index.jsx
@@ -5,19 +5,35 @@ import Layout from '../../Components/Layout'
 
 function SignIn() {
     const context = React.useContext(Context)
-
+    const [view, setView] = React.useState('user-info')
+    const form = React.useRef(null)
+    
     // Account
     const account = localStorage.getItem('account')
     const parsedAccount = JSON.parse(account)
-
+    
     // Has an account
     const NoAccountInLocalStorage = parsedAccount ? Object.keys(parsedAccount).length === 0: true
     const NoAccountInLocalState = context.account ? Object.keys(context.account).length === 0: true
     const hasUserAnAccount = !NoAccountInLocalStorage || !NoAccountInLocalState
+    
+    function createAnAccount() {
+        const formData = new FormData(form.current)
+        const data = {
+            name: formData.get('name'),
+            email: formData.get('email'),
+            password: formData.get('password')
+        }
+        let stringifiedData = JSON.stringify(data)
+        localStorage.setItem('account', stringifiedData)
+        context.setAccount(data)
+        console.log(data)
+    }
 
-    return (
-        <Layout>
-            <h1 className='font-medium text-xl text-center mb-6 w-80'>Welcome</h1>
+
+
+    function renderLogIn() {
+        return (
             <div className='flex flex-col w-80'>
                 <p>
                     <span className='font-light text-sm'>Email: </span>
@@ -40,11 +56,74 @@ function SignIn() {
                 </div>
                 <button
                     className='border border-black disabled:text-black/40 disabled:border-black/40 rounded-lg mt-6 py-3'
+                    onClick={() => setView('create-user-info')}
                     disabled={hasUserAnAccount}>
                         Sing up
                 </button>
             </div>
-        </Layout>
+        )
+    }
+
+    function renderCreateUserInfo() {
+        return (
+            <form ref={form} className='flex flex-col gap-4 w-80' >
+                <div className='flex flex-col gap-1'>
+                    <label htmlFor="name" className='font-light text-sm'>Your name: </label>
+                    <input 
+                        type="text"
+                        id='name'
+                        name='name'
+                        defaultValue={parsedAccount?.name}
+                        placeholder='Peter'
+                        className='rounded-lg border border-black placeholder:font-light placeholder:text-sm placeholder:text-black/60 focus:outline-none py-2 px-4' />
+                </div>
+                <div className='flex flex-col gap-1'>
+                    <label htmlFor="email" className='font-light text-sm'>Your email: </label>
+                    <input 
+                        type="text"
+                        id='email'
+                        name='email'
+                        defaultValue={parsedAccount?.email}
+                        placeholder='peruanos@enelmundo.pe'
+                        className='rounded-lg border border-black placeholder:font-light placeholder:text-sm placeholder:text-black/60 focus:outline-none py-2 px-4' />
+                </div>
+                <div className='flex flex-col gap-1'>
+                    <label htmlFor="password" className='font-light text-sm'>Your password:</label>
+                    <input 
+                        type="text"
+                        id='password'
+                        name='password'
+                        defaultValue={parsedAccount?.password}
+                        placeholder='*********'
+                        className='rounded-lg border border-black placeholder:font-light placeholder:text-sm placeholder:text-black/60 focus:outline-none py-2 px-4' />
+                </div>
+                <Link
+                    to='/'>
+                    <button
+                        className='bg-green-500 text-white w-full rounded-lg py-3'
+                        onClick={() => createAnAccount()}>
+                        Create
+                    </button>
+                </Link>
+            </form>
+        )
+    }
+    
+    // const renderView = () => view ==='create-user-info' ? renderCreateUserInfo() : renderLogIn()
+    
+    function renderView() {
+        if (view ==='create-user-info') {
+                return renderCreateUserInfo()
+            } else {
+                return renderLogIn()
+                }
+            }
+            
+            return (
+                <Layout>
+                    <h1 className='font-medium text-xl text-center mb-6 w-80'>Welcome</h1>
+                    {renderView()}
+                </Layout>
     )
 }
 
