8c409661d68ea5121bccd5eb3af9963424955898
Juan Diego Salvador <juandiegosalvador@gmail.com>
2023-06-04 11:20:15 -0500

    final reto2: sign in

--- src/Components/Navbar/index.jsx
+++ src/Components/Navbar/index.jsx
@@ -7,10 +7,60 @@ function Navbar() {
     const context = React.useContext(Context)
     const activeStyle = "underline underline-offset-4"
 
+    // Sign out
+    const signout = localStorage.getItem('sign-out')
+    const parsedSignout = JSON.parse(signout)
+    const isUserSignout = context.signout || parsedSignout
+
     function handleSignout() {
         const stringifiedSignout = JSON.stringify(true)
         localStorage.setItem('sign-out', stringifiedSignout)
         context.setSignout(true)
+    }
+
+    function renderView() {
+        if (isUserSignout) {
+            return (
+                <li>
+                    <NavLink
+                        to='/sign-in'
+                        className={({ isActive }) => isActive ? activeStyle : undefined}
+                        onClick={() => handleSignout()}>Sign In</NavLink>
+                </li>
+            )
+        } else {
+            return (
+                <>
+                    <li className="text-black/60">
+                        j.salvadorp@platzi.com
+                    </li>
+                    <li>
+                        <NavLink
+                            to='/my-orders'
+                            onClick={() => context.setSearchByCategory()}
+                            className={({ isActive }) => isActive ? activeStyle : undefined}>My Orders</NavLink>
+                    </li>
+                    <li>
+                        <NavLink
+                            to='/my-account'
+                            onClick={() => context.setSearchByCategory()}
+                            className={({ isActive }) => isActive ? activeStyle : undefined}>My Account</NavLink>
+                    </li>
+                    <li>
+                        <NavLink
+                            to='/sign-in'
+                            className={({ isActive }) => isActive ? activeStyle : undefined}
+                            onClick={() => handleSignout()}>Sign Out</NavLink>
+                    </li>
+                    <li className='flex items-center justify-center'>
+                        <ShoppingCartIcon className='h-6 w-6 text-black' />
+                        <div>
+                            {context.cartProducts.length}
+                        </div>
+                    </li>
+                </>
+            )
+        }
     }
 
     return (
@@ -19,107 +69,50 @@ function Navbar() {
                 <li className="font-semibold text-lg">
                     <NavLink 
                         to='/'
-                        onClick={()=>context.setSearchByCategory()}
-                    >
-                        Shopi
-                    </NavLink>
+                        onClick={()=>context.setSearchByCategory()}>Shopi</NavLink>
                 </li>
                 <li>
                     <NavLink 
                         to='/'
                         onClick={()=>context.setSearchByCategory()}
-                        className={({ isActive }) => isActive ? activeStyle : undefined}
-                    >
-                        All
-                    </NavLink>
+                        className={({ isActive }) => isActive ? activeStyle : undefined}>All</NavLink>
                 </li>
                 <li>
                     <NavLink 
                         to='/clothes'
                         onClick={()=>context.setSearchByCategory('Clothes')}
-                        className={({ isActive }) => isActive ? activeStyle : undefined}
-                    >
-                        Clothes
-                    </NavLink>
+                        className={({ isActive }) => isActive ? activeStyle : undefined}>Clothes</NavLink>
                 </li>
                 <li>
                     <NavLink 
                         to='/electronics'
                         onClick={()=>context.setSearchByCategory('Electronics')}
-                        className={({ isActive }) => isActive ? activeStyle : undefined}
-                    >
-                        Electronics
-                    </NavLink>
+                        className={({ isActive }) => isActive ? activeStyle : undefined}>Electronics</NavLink>
                 </li>
                 <li>
                     <NavLink 
                         to='/furnitures'
                         onClick={()=>context.setSearchByCategory('Furniture')}
-                        className={({ isActive }) => isActive ? activeStyle : undefined}
-                    >
-                        Furnitures
-                    </NavLink>
+                        className={({ isActive }) => isActive ? activeStyle : undefined}>Furnitures</NavLink>
                 </li>
                 <li>
                     <NavLink 
                         to='/Toys'
                         onClick={()=>context.setSearchByCategory('toys')}
-                        className={({ isActive }) => isActive ? activeStyle : undefined}
-                    >
-                        Toys
-                    </NavLink>
+                        className={({ isActive }) => isActive ? activeStyle : undefined}>Toys</NavLink>
                 </li>
                 <li>
                     <NavLink 
                         to='/Others'
                         onClick={()=>context.setSearchByCategory('Others')}
-                        className={({ isActive }) => isActive ? activeStyle : undefined}
-                    >
-                        Others
-                    </NavLink>
+                        className={({ isActive }) => isActive ? activeStyle : undefined}>Others</NavLink>
                 </li>
             </ul>
             <ul className="flex items-center gap-3">
-                <li className="text-black/60">
-                    j.salvadorp@platzi.com
-                </li>
-                <li>
-                    <NavLink 
-                    to='/my-orders'
-                        onClick={()=>context.setSearchByCategory()}
-                        className={({ isActive }) => isActive ? activeStyle : undefined}
-                    >
-                        My Orders
-                    </NavLink>
-                </li>
-                <li>
-                    <NavLink 
-                        to='/my-account'
-                        onClick={()=>context.setSearchByCategory()}
-                        className={({ isActive }) => isActive ? activeStyle : undefined}
-                    >
-                        My Account
-                    </NavLink>
-                </li>
-                <li>
-                    <NavLink 
-                        to='/sign-in'
-                        className={({ isActive }) => isActive ? activeStyle : undefined}
-                        onClick={()=>handleSignout()}
-                    >
-                        Sign Out
-                    </NavLink>
-                </li>
-                <li className='flex items-center justify-center'>
-                    <ShoppingCartIcon className='h-6 w-6 text-black'/>
-                    <div>
-                        {context.cartProducts.length}
-                    </div>
-                </li>
+                {renderView()}
             </ul>
         </nav>
-
-    )
+    )        
 
 }
 
--- src/Pages/SignIn/index.jsx
+++ src/Pages/SignIn/index.jsx
@@ -1,9 +1,34 @@
+import { Link } from 'react-router-dom'
 import Layout from '../../Components/Layout'
 
 function SignIn() {
     return (
         <Layout>
-            Sign In
+            <h1 className='font-medium text-xl text-center mb-6 w-80'>Welcome</h1>
+            <div className='flex flex-col w-80'>
+                <p>
+                    <span className='font-light text-sm'>Email: </span>
+                    <span>juandiego@platzi.com</span>
+                </p>
+                <p>
+                    <span className='font-light text-sm'>Password: </span>
+                    <span>***********</span>
+                </p>
+                <Link
+                    to='/'>
+                    <button
+                        className='bg-green-500 disabled:bg-green-500/40 text-white w-full rounded-lg py-3 mt-4 mb-2'>
+                        Log in
+                    </button>
+                </Link>
+                <div className='text-center'>
+                    <a className='font-light text-xs underline underline-offset-4' href="/"></a>
+                </div>
+                <button
+                    className='border border-black disabled:text-black/40 disabled:border-black/40 rounded-lg mt-6 py-3'>
+                        Sing up
+                </button>
+            </div>
         </Layout>
     )
 }
